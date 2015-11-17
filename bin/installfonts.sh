33374430fb69f484aca3836410781ecb0cae7506
Yegor Dolgopolov <quantumdark@gmail.com>
2015-11-17 11:17:40 -0500

    Change FYI the path

--- bin/installfonts.sh
+++ bin/installfonts.sh
@@ -4,6 +4,6 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 mkdir -p ~/.fonts
 
-curl -#L "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/LiberationMono/Literation%20Mono%20Powerline%20Plus%20Nerd%20File%20Types%20Mono%20Plus%20Pomicons.ttf" -o ~/.fonts/LiterationMonoPlusIcons.ttf
+curl -#L "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/LiberationMono/additional-variations/Literation%20Mono%20Powerline%20Nerd%20Font%20Plus%20Font%20Awesome%20Plus%20Octicons%20Plus%20Pomicons%20Mono.ttf" -o ~/.fonts/LiterationMonoPlusIcons.ttf
 
 fc-cache -vf ~/.fonts/
