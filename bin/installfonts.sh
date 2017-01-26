7e9f3b6ca58c8c97f295b9bd29dc74248a17c9c0
Yegor Dolgopolov <quantumdark@gmail.com>
2017-01-26 19:32:57 +0300

    Changed font location

--- bin/installfonts.sh
+++ bin/installfonts.sh
@@ -4,6 +4,6 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 mkdir -p ~/.fonts
 
-curl -#L "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/LiberationMono/additional-variations/Literation%20Mono%20Powerline%20Nerd%20Font%20Plus%20Font%20Awesome%20Plus%20Octicons%20Plus%20Pomicons%20Mono.ttf" -o ~/.fonts/LiterationMonoPlusIcons.ttf
+curl -#L "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/LiberationMono/complete/Literation%20Mono%20Powerline%20Nerd%20Font%20Complete%20Mono.ttf?raw=true" -o ~/.fonts/LiterationMonoPowerlineNerd.ttf
 
 fc-cache -vf ~/.fonts/
